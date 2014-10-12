32a5fb248688a78eb88f6caf95a87e5d6a3d59f8
Dan Riti <dmriti@gmail.com>
2014-10-12 11:29:05 -0700

    Update to add vim plugins.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,7 +1,26 @@
-default["halt"]["directories"] = [
-  "bin", 
-  "dev", 
-  "opt", 
-  "tmp", 
-  "work"
+default["halt"]["home_directories"] = [
+  "/home/driti/bin", 
+  "/home/driti/dev", 
+  "/home/driti/opt", 
+  "/home/driti/tmp", 
+  "/home/driti/work",
+]
+
+default["halt"]["pathogen_directories"] = [
+  "/home/driti/.vim",
+  "/home/driti/.vim/autoload",
+  "/home/driti/.vim/bundle",
+]
+
+default["halt"]["vim_plugins"] = [
+  ["supertab",          "https://github.com/ervandew/supertab.git"],
+  ["syntastic",         "https://github.com/scrooloose/syntastic.git"],
+  ["vim-coffee-script", "https://github.com/kchmck/vim-coffee-script.git"],
+  ["vim-fugitive",      "https://github.com/tpope/vim-fugitive.git"],
+  ["vim-go",            "https://github.com/fatih/vim-go.git"],
+  ["vim-javascript",    "https://github.com/pangloss/vim-javascript.git"],
+  ["vim-less",          "https://github.com/groenewege/vim-less.git"],
+  ["vim-markdown",      "https://github.com/plasticboy/vim-markdown.git"],
+  ["vim-ruby",          "https://github.com/vim-ruby/vim-ruby.git"],
+  ["xoria256",          "https://github.com/vim-scripts/xoria256.vim"]
 ]
--- recipes/default.rb
+++ recipes/default.rb
@@ -36,8 +36,8 @@ group "ubuntu" do
   members ["ubuntu", "driti"]
 end
 
-node['halt']['directories'].each do |dir|
-  directory "/home/driti/#{dir}" do
+node['halt']['home_directories'].each do |dir|
+  directory "#{dir}" do
     owner 'driti'
     group 'driti'
   end
@@ -50,10 +50,35 @@ git "/home/driti/dev/config" do
   group "driti"
 end
 
+
+# VIM
+# ---------------------
+node['halt']['pathogen_directories'].each do |dir|
+  directory "#{dir}" do
+    owner 'driti'
+    group 'driti'
+  end
+end
+
 remote_file "/home/driti/.vimrc" do
   source "file:///home/driti/dev/config/.vimrc"
   user "driti"
   group "driti"
+end
+
+remote_file "/home/driti/.vim/autoload/pathogen.vim" do
+  source "https://tpo.pe/pathogen.vim"
+  user "driti"
+  group "driti"
+end
+
+node["halt"]["vim_plugins"].each do |plugin|
+  git "/home/driti/.vim/bundle/#{plugin[0]}" do
+    repository "#{plugin[1]}"
+    revision "master"
+    user "driti"
+    group "driti"
+  end
 end
 
 
